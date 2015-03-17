c65f159260b28d503f7a27ea5c7e77e1f0677fb0
Borna Čobanov <bcobanov@foi.hr>
2015-03-17 16:50:59 +0100

    https://github.com/bcobanovfoi/PrviRepozitorij.git

---
+++ Skripta zadatak 11/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skripta_zadatak_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            NovaKlasa nova = new NovaKlasa();
+
+            string a = textBox1.Text;
+            string b = textBox2.Text;
+
+            int brojac = nova.getUppers(a);
+
+            nova.Logika(a, b, this, brojac);
+
+
+
+
+        }
+    }
+}
---
+++ Skripta zadatak 11/NovaKlasa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skripta_zadatak_11
+{
+    class NovaKlasa
+    {
+
+     internal int getUppers(string a)
+        {
+            int brojac = 0;
+            foreach (char c in a)
+            {
+                if (char.IsUpper(c))
+                {
+                    brojac++;
+                }
+            }
+            return brojac;
+        }
+
+   
+     
+    internal void Logika(string a,string b,Form1 form1, int brojac)
+    {
+
+            if (a != b)
+            {
+                form1.button1.Text = "Razlicite";
+            }
+            else if (a.Length < 5)
+            {
+                form1.button1.Text = "< 5";
+            }
+            else if (brojac < 3)
+            {
+                form1.button1.Text = "<3 velika";
+            }
+            else if (!a.Contains('#') && !a.Contains('$') && !a.Contains('%'))
+            {
+                form1.button1.Text = "nema retardiranih";
+            }
+            else
+            {
+                form1.pictureBox1.Show();
+                form1.button1.Text = "5 bani buraz";
+
+            }
+    
+    }
+    
+    }
+    }
+
